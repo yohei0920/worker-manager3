862975bbb1c0df98f3fc553cabf921c0ef7f5441
unknown <yohei.hokari@gmail.com>
2023-04-13 21:48:46 +0900

    devise追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   post "/graphql", to: "graphql#execute"
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
